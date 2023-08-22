3becce1cbb70e32bdb279487f755618186031528
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-22 16:02:19 +0800

    feat: Implemented get all employee , added employee class, created exception, and created employee repository

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping(path = "employees")
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> listAll() {
+        return employeeRepository.listAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Employee findById(@PathVariable Long id){
+        return employeeRepository.findById(id);
+    }
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeRepository.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+    private static final List<Employee> employees = new ArrayList<>();
+
+    static {
+        employees.add(new Employee(1l, "Ilnear", 42, "Male", 10000));
+        employees.add(new Employee(2l, "Ilfar", 42, "Female", 20000));
+        employees.add(new Employee(3l, "Ilclose", 42, "Male", 3000));
+        employees.add(new Employee(4l, "Ilalmostthere", 42, "Female", 5000));
+        employees.add(new Employee(5l, "Ilfaraway", 42, "Male", 14500));
+    }
+
+    public List<Employee> listAll() {
+        return employees;
+    }
+
+    public Employee findById(Long id) {
+        return employees.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst()
+                .orElseThrow(EmployeeNotFoundException::new);
+    }
+
+}
