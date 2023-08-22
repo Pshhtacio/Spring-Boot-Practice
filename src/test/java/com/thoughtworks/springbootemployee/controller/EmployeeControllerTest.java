a0241037ccce6dcbd41eedc69d1ffd402b60e1b6
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-22 23:23:51 +0800

    test: add test suite for the employee controller

---
+++ src/test/java/com/thoughtworks/springbootemployee/controller/EmployeeControllerTest.java
@@ -0,0 +1,35 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmployeeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByGender() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void findByPage() {
+    }
+}
