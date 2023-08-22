f0dc64b75a2b21145604de9cc1440434c03a3933
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-23 01:09:38 +0800

    feat: add Company controller with springframework annotations

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(path = "companies")
+@RestController
+public class CompanyController {
+
+    private final CompanyRepository companyRepository;
+
+    @Autowired
+    public CompanyController(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+}
