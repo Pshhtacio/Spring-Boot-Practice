067ffff6c52ee8fc1c3dbf52b37c4719dd504b7b
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-22 23:29:14 +0800

    test: add test for listAll method

--- src/test/java/com/thoughtworks/springbootemployee/controller/EmployeeControllerTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/controller/EmployeeControllerTest.java
@@ -1,16 +1,45 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class EmployeeControllerTest {
 
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeController employeeController;
+
     @BeforeEach
-    void setUp() {
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void listAll() {
+    public void given_employees_when_listAll__then_list_of_employees_is_Returned() {
+        // Given
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1L, "John", 30, "Male", 50000));
+        employees.add(new Employee(2L, "Alice", 25, "Female", 60000));
+        when(employeeRepository.listAll()).thenReturn(employees);
+
+        // When
+        List<Employee> result = employeeController.listAll();
+
+        // Then
+        assertEquals(employees, result);
     }
 
     @Test
