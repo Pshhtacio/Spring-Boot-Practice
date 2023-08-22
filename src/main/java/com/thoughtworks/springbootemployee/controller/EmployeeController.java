2ea25e45ea5ca54c9c4f68361ec871cdb4a60485
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-22 17:02:37 +0800

    feat: Implemented add new Employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,8 +10,13 @@ import java.util.List;
 @RestController
 public class EmployeeController {
 
+
+    private final EmployeeRepository employeeRepository;
+
     @Autowired
-    private EmployeeRepository employeeRepository;
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @GetMapping
     public List<Employee> listAll() {
@@ -18,13 +24,18 @@ public class EmployeeController {
     }
 
     @GetMapping(path = "/{id}")
-    public Employee findById(@PathVariable Long id){
+    public Employee findById(@PathVariable Long id) {
         return employeeRepository.findById(id);
     }
 
     @GetMapping(params = {"gender"})
-        public List<Employee> findByGender(@RequestParam String gender) {
+    public List<Employee> findByGender(@RequestParam String gender) {
         return employeeRepository.findByGender(gender);
-        }
+    }
 
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee addEmployee(@RequestBody Employee employee){
+        return employeeRepository.addEmployee(employee);
+    }
 }
